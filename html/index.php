f01d351882d1e7a18f3e53cef21feebba9ca44fe
Fernando Casaliba Perlangeli <fernando.casaliba@gmail.com>
2016-02-27 00:29:33 -0300

    Revert "Revert "Suamando archivos""
    This reverts commit a5fb89ab8a2cf827e3ad516c48f50169484fd926.

---
+++ html/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="author" content="ayrezsolutions.com">
+	<title>Avalon</title>
+
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="styles/animate.css" rel="stylesheet" type="text/css" />
+	<link href="styles/normalize.css" rel="stylesheet" type="text/css" />
+	<link href="styles/styles.css" rel="stylesheet"  type="text/css"  />
+</head>
+<body>
+<div class="cFull js clearfix m0 p0">
+	<!-- <div id="preloader" class="preloader_individuos"></div> -->
+	<!-- fin preloader -->
+
+	<header class="header">
+		<div id="car-head" class="carousel slide" data-ride="carousel" data-interval="false">		   
+		    <div class="carousel-inner">	       
+	        <div class="item active">
+	          <img class="img-responsive col-xs-12 p-0" src="http://www.personal.com.ar/images/individuos/home/h1_McAfee.jpg" />
+	        </div>
+	        <div class="item">
+	          <img class="img-responsive col-xs-12 p-0" src="http://www.personal.com.ar/images/individuos/home/superchip_abril.jpg" />
+	        </div>
+	        <div class="item">
+	          <img class="img-responsive col-xs-12 p-0" src="http://www.personal.com.ar/images/individuos/tienda/h1-smsgrupal.jpg" />
+	        </div>
+	        <div class="item">
+	          <img class="img-responsive col-xs-12 p-0" src="http://www.personal.com.ar/images/individuos/home/h1_spotify_martes.jpg" />
+	        </div>
+	        <div class="item">
+	          <img class="img-responsive col-xs-12 p-0" src="http://www.personal.com.ar/images/individuos/home/H1_nadie_lo_hace_como_vos_web-2.jpg" />
+	        </div>
+		    </div>
+		    <a class="left carousel-control" href="#car-head" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		    <a class="right carousel-control" href="#car-head" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+
+		    <ol class="carousel-indicators">
+	        <li data-target="#car-head" data-slide-to="0" class="active"></li>
+	        <li data-target="#car-head" data-slide-to="1" class=""></li>
+	        <li data-target="#car-head" data-slide-to="2" class=""></li>
+	        <li data-target="#car-head" data-slide-to="3" class=""></li>
+	        <li data-target="#car-head" data-slide-to="4" class=""></li>
+		    </ol>
+		</div>
+	</header>
+	<!-- fin header -->
+
+	<section class="seccion">
+		<div class="container">
+			<div class="row modulo">
+				<article class="col-xs-12">
+				</article>
+				<!-- fin -->
+			</div>
+			<!-- fin row -->
+		</div>
+	</section>
+	<!-- fin seccion -->
+
+</div>
+<!-- js -->
+
+<script type='text/javascript' src='scripts/jquery.js'></script>
+<script type='text/javascript' src='scripts/bootstrap.min.js'></script>
+<script type='text/javascript' src="scripts/modernizr.js"></script>
+<script type='text/javascript' src="scripts/easing.js"></script>
+<!--[if lt IE 9]>
+	<script src="scripts/html5shiv.js"></script>
+	<script src="scripts/respond.js"></script>
+<![endif]-->
+<script type='text/javascript' src='scripts/acciones.js'></script>
+<script type='text/javascript' src="scripts/wow.js"></script>
+<script>
+	new WOW().init();
+	// fin wow
+</script>
+
+<div class="toolresponsive">
+	<span class="visible-lg txc">LG</span>
+	<span class="visible-md txc">MD</span>
+	<span class="visible-sm txc">SM</span>
+	<span class="visible-xs txc">XS</span>
+</div>
+
+</body>
+</html>
