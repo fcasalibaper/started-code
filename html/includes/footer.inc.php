45f693f0d0f6b8ce816c3fef12535d1908c28658
Fernando Casaliba Perlangeli <fernando.casaliba@gmail.com>
2016-02-26 23:33:44 -0300

    Revert "Sumando archivos"
    This reverts commit d17816fff5f87e6a22aa1df4f6e84fdd23108a2d.

--- html/includes/footer.inc.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer col-xs-12 col-sm-offset-4 col-sm-4">
-	<p>scroll</p>
-	<i class="iconfooter">
-			<?php include 'logo.inc.php'; ?>
-	</i>
-	<p>down</p>
-</footer>
-<!-- fin footer -->
